80c8e19f183ad2144a083261b954c74a8ee83ba9
Erik Sjölund <erik.sjolund@gmail.com>
2021-01-02 21:10:19 +0100

    Fix a few bugs. It works now.
        $ curl -s http://localhost:10000
        Connected to Postgres, version: PostgreSQL 12.5 (Debian 12.5-1.pgdg100+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 8.3.0-6) 8.3.0, 64-bit Connected to Postgres, version: PostgreSQL 13.1 (Debian 13.1-1.pgdg100+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 8.3.0-6) 8.3.0, 64-bit
    
    Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

--- examples/double-proxy/service.py
+++ examples/double-proxy/service.py
@@ -4,23 +4,22 @@ from flask import Flask
 
 import psycopg2
 
-app = Flask(__name__)
-
-
-@app.route('/')
-
 def connect_to_postgres(host, port):
-  conn = psycopg2.connect(f"host={host} user=postgres port={port}")
+  conn = psycopg2.connect("host={} user=postgres port={}".format(host, port))
   cur = conn.cursor()
   cur.execute('SELECT version()')
   msg = 'Connected to Postgres, version: %s' % cur.fetchone()
   cur.close()
+  conn.close()
   return msg
 
+app = Flask(__name__)
+
+@app.route('/')
 def hello():
   msg = ''
-  msg = msg + connect_to_postgres('proxy-postgres-frontend.example.com', 5432)
-  msg = msg + connect_to_postgres('proxy-postgres-frontend.example.com', 5433)
+  msg = msg + connect_to_postgres('proxy-postgres-frontend', 5432) + " "
+  msg = msg + connect_to_postgres('proxy-postgres-frontend', 5433)
   return msg
 
 if __name__ == "__main__":

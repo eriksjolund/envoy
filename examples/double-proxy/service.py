953b7d4978d0916c49def869f67344ad68f719bc
Erik Sjölund <erik.sjolund@gmail.com>
2021-01-02 18:28:13 +0100

    Modify double-proxy example so that it uses mTLS (SNI) to serve two different postgres backends
                                                                                                                                                                                                                                         +-------------------------+
                                                                                                                                                                                                                                         |                         |
                                                                                                                                                                                                                                  +------| postgres-1 (version 12) |
        +----------------+      +------------+        port 5432 +-------------------------+                                        mTLS (SNI)                                         +-----------------------------------+       |      |                         |
        |                |      |            |----------------->|                         |  proxy-postgres-frontend-1.example.com.csr --> proxy-postgres-backend-1.example.com.csr   |                                   |-------+      +-------------------------+
        | Envoy (front)  |----->|   Flask    |        port 5433 | Envoy (postgres-front)  |------------------------------------------------------------------------------------------>|    Envoy (postgres-backend)       |
        |                |      |            |----------------->|                         |  proxy-postgres-frontend-2.example.com.csr --> proxy-postgres-backend-2.example.com.csr   |                                   |-------+      +-------------------------+
        +----------------+      +------------+                  +-------------------------+                                                                                           +-----------------------------------+       |      |                         |
                                                                                                                                                                                                                                  +------| postgres-2 (version 13) |
                                                                                                                                                                                                                                         |                         |
                                                                                                                                                                                                                                         +-------------------------+
    
    Signed-off-by: Erik Sjölund <erik.sjolund@gmail.com>

--- examples/double-proxy/service.py
+++ examples/double-proxy/service.py
@@ -8,14 +8,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-  conn = psycopg2.connect("host=postgres user=postgres")
+
+def connect_to_postgres(host, port):
+  conn = psycopg2.connect(f"host={host} user=postgres port={port}")
   cur = conn.cursor()
   cur.execute('SELECT version()')
   msg = 'Connected to Postgres, version: %s' % cur.fetchone()
   cur.close()
   return msg
 
+def hello():
+  msg = ''
+  msg = msg + connect_to_postgres('proxy-postgres-frontend.example.com', 5432)
+  msg = msg + connect_to_postgres('proxy-postgres-frontend.example.com', 5433)
+  return msg
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8000, debug=True)
